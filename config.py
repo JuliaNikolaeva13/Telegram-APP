29d05f1068284bbbaceb813b60cda88380dc4e56
DESKTOP-9021RLV\Юля <yulika3333@mail.ru>
2023-10-23 21:26:05 +0300

    Initial commit

---
+++ config.py
@@ -0,0 +1,11 @@
+TOKEN = '6756451584:AAEsQbuo1LPCmvKEY7uEdu2fgFow18MNZ1k'
+
+keys = {
+    'рубль': 'RUB',
+    'евро': 'EUR',
+    'доллар': 'USD',
+}
+
+# 'биткоин': 'BTC',
+# 'эфириум': 'ETH',
+# 'доллар': 'USD',
